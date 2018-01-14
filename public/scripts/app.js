99a9c7e729cb8eafe71bf62ce6657ba753d130ff
Asim Abdelgadir <acsaasim@hotmail.com>
2018-01-14 00:14:31 -0500

    forgot to create package.json

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,18 @@
 console.log('app.js is running')
 
-//JSX
+////JSX - Requires Babel and webpack
+//var template = <p>This is JSX! from app.js</p>;
+
+////Plain JS:
+"use strict";
+
+var template = React.createElement(
+  "p",
+  { id: "some id here" },
+  "This is Plain JS! from app.js"
+);
+
+//These will run with or without Babel
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
