427aa192145432923c28d8f8d854e340796f4fdc
Asim Abdelgadir <acsaasim@hotmail.com>
2018-01-14 03:35:21 -0500

    adding date and time functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,23 +2,78 @@
 
 console.log('app.js is running');
 
-////JSX - Requires Babel and webpack
-var template = React.createElement(
-  'p',
-  null,
-  'This is JSX! from app.js - using watch'
+var userDetailsName = 'Asim';
+var userDetailAge = 45;
+var userDetailsOccupation = 'DevOps Engineer';
+
+var myToday = new Date();;
+var month = new Array(12);
+month[0] = "Jan";
+month[1] = "Feb";
+month[2] = "Mar";
+month[3] = "Apr";
+month[4] = "May";
+month[5] = "Jun";
+month[6] = "Jul";
+month[7] = "Aug";
+month[8] = "Sep";
+month[9] = "Oct";
+month[10] = "Nov";
+month[11] = "Dec";
+
+var myDay = new Array(7);
+myDay[0] = "Sun";
+myDay[1] = "Mon";
+myDay[2] = "Tue";
+myDay[3] = "Wed";
+myDay[4] = "Thu";
+myDay[5] = "Fri";
+myDay[6] = "Sat";
+
+var userDetailsAsOf1 = myToday.getDay() + ' ' + myToday.getDate() + ' - ' + (myToday.getMonth() + 1) + ' - ' + myToday.getFullYear();
+var userDetailsAsOf2 = myDay[myToday.getDay()] + ' ' + myToday.getDate() + ' - ' + month[myToday.getUTCMonth()] + ' - ' + myToday.getFullYear();
+
+var myBioTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'My Bio'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Name: ',
+        userDetailsName.toUpperCase() + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        userDetailAge
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Occupation: ',
+        userDetailsOccupation
+    ),
+    React.createElement(
+        'p',
+        null,
+        'As of: ',
+        userDetailsAsOf1
+    ),
+    React.createElement(
+        'p',
+        null,
+        'As of: ',
+        userDetailsAsOf2
+    )
 );
-
-////Plain JS:
-// "use strict";
-
-// var template = React.createElement(
-//   "p",
-//   { id: "some id here" },
-//   "This is Plain JS! from app.js"
-// );
 
 //These will run with or without Babel
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(myBioTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,18 +1,50 @@
+
 console.log('app.js is running')
 
-////JSX - Requires Babel and webpack
-var template = <p>This is JSX! from app.js - using watch</p>;
+var userDetailsName = 'Asim';
+var userDetailAge = 45;
+var userDetailsOccupation = 'DevOps Engineer';
 
-////Plain JS:
-// "use strict";
+var myToday = new Date();;
+var month = new Array(12);
+month[0] = "Jan";
+month[1] = "Feb";
+month[2] = "Mar";
+month[3] = "Apr";
+month[4] = "May";
+month[5] = "Jun";
+month[6] = "Jul";
+month[7] = "Aug";
+month[8] = "Sep";
+month[9] = "Oct";
+month[10] = "Nov";
+month[11] = "Dec";
 
-// var template = React.createElement(
-//   "p",
-//   { id: "some id here" },
-//   "This is Plain JS! from app.js"
-// );
+var myDay = new Array(7);
+myDay[0] = "Sun";
+myDay[1] = "Mon";
+myDay[2] = "Tue";
+myDay[3] = "Wed";
+myDay[4] = "Thu";
+myDay[5] = "Fri";
+myDay[6] = "Sat";
+
+
+var userDetailsAsOf1 = myToday.getDay() + ' ' + myToday.getDate() + ' - ' + (myToday.getMonth() + 1) + ' - ' + myToday.getFullYear();
+var userDetailsAsOf2 = myDay[myToday.getDay()] + ' ' + myToday.getDate() + ' - ' + month[myToday.getUTCMonth()] + ' - ' + myToday.getFullYear();
+
+var myBioTemplate = (
+    <div>
+        <h1>My Bio</h1>
+        <p>Name: {userDetailsName.toUpperCase() + '!'}</p>
+        <p>Age: {userDetailAge}</p>
+        <p>Occupation: {userDetailsOccupation}</p>
+        <p>As of: {userDetailsAsOf1}</p>
+        <p>As of: {userDetailsAsOf2}</p>
+    </div>
+    );
 
 //These will run with or without Babel
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(myBioTemplate, appRoot);
