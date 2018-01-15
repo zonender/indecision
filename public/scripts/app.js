f7174f05811f817343920644249c9479b61cdaf4
Asim Abdelgadir <acsaasim@hotmail.com>
2018-01-15 00:01:35 -0500

    using let const and arrow functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,31 @@
 'use strict';
 
+//babel command
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react
+
+//babel watch command
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+//live-server command, public is the folder containing the files u want to serve
+//live-server public
+
 console.log('app.js is running');
 
-var userDetailsName = 'Asim';
-var userDetailAge = 45;
-var userDetailsOccupation = 'DevOps Engineer';
+var appObj = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['one', 'two']
+};
 
-var myToday = new Date();;
+var bioObj = {
+    name: "Asim",
+    age: 45,
+    occupation: "DevOps Engineer",
+    Location1: 'New York',
+    Location2: 'New Jersey'
+};
+
+var myToday = new Date();
 var month = new Array(12);
 month[0] = "Jan";
 month[1] = "Feb";
@@ -33,7 +52,73 @@ myDay[6] = "Sat";
 var userDetailsAsOf1 = myToday.getDay() + ' ' + myToday.getDate() + ' - ' + (myToday.getMonth() + 1) + ' - ' + myToday.getFullYear();
 var userDetailsAsOf2 = myDay[myToday.getDay()] + ' ' + myToday.getDate() + ' - ' + month[myToday.getUTCMonth()] + ' - ' + myToday.getFullYear();
 
-var myBioTemplate = React.createElement(
+// function getLocation (location) {
+//     if (location) {
+//         return location;
+//     } else {
+//         return 'Unknown'
+//     };
+// };
+
+//using an arrow function
+var getLocation1 = function getLocation1(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location1: ',
+            location
+        );
+    }
+};
+
+//using a regular function
+function getLocation2(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location2: ',
+            location
+        );
+    }
+}
+
+var appTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appObj.title
+    ),
+    appObj.subtitle && React.createElement(
+        'p',
+        null,
+        appObj.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        appObj.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item two'
+        )
+    )
+);
+
+var bioTemplate = React.createElement(
     'div',
     null,
     React.createElement(
@@ -45,20 +130,22 @@ var myBioTemplate = React.createElement(
         'p',
         null,
         'Name: ',
-        userDetailsName.toUpperCase() + '!'
+        bioObj.name ? bioObj.name.toUpperCase() + '!' : 'Anonymous'
     ),
-    React.createElement(
+    bioObj.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
-        userDetailAge
+        bioObj.age
     ),
     React.createElement(
         'p',
         null,
         'Occupation: ',
-        userDetailsOccupation
+        bioObj.occupation
     ),
+    getLocation1(bioObj.Location1),
+    getLocation2(bioObj.Location2),
     React.createElement(
         'p',
         null,
@@ -73,7 +160,6 @@ var myBioTemplate = React.createElement(
     )
 );
 
-//These will run with or without Babel
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(myBioTemplate, appRoot);
+ReactDOM.render(bioTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
+//babel command
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react
+
+//babel watch command
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+//live-server command, public is the folder containing the files u want to serve
+//live-server public
 
 console.log('app.js is running')
 
-var userDetailsName = 'Asim';
-var userDetailAge = 45;
-var userDetailsOccupation = 'DevOps Engineer';
+const appObj = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['one', 'two']
+};
 
-var myToday = new Date();;
-var month = new Array(12);
+const bioObj = {
+    name: "Asim",
+    age: 45,
+    occupation: "DevOps Engineer",
+    Location1: 'New York',
+    Location2: 'New Jersey'
+};
+
+const myToday = new Date();
+const month = new Array(12);
 month[0] = "Jan";
 month[1] = "Feb";
 month[2] = "Mar";
@@ -20,7 +38,7 @@ month[9] = "Oct";
 month[10] = "Nov";
 month[11] = "Dec";
 
-var myDay = new Array(7);
+const myDay = new Array(7);
 myDay[0] = "Sun";
 myDay[1] = "Mon";
 myDay[2] = "Tue";
@@ -30,21 +48,56 @@ myDay[5] = "Fri";
 myDay[6] = "Sat";
 
 
-var userDetailsAsOf1 = myToday.getDay() + ' ' + myToday.getDate() + ' - ' + (myToday.getMonth() + 1) + ' - ' + myToday.getFullYear();
-var userDetailsAsOf2 = myDay[myToday.getDay()] + ' ' + myToday.getDate() + ' - ' + month[myToday.getUTCMonth()] + ' - ' + myToday.getFullYear();
+const userDetailsAsOf1 = myToday.getDay() + ' ' + myToday.getDate() + ' - ' + (myToday.getMonth() + 1) + ' - ' + myToday.getFullYear();
+const userDetailsAsOf2 = myDay[myToday.getDay()] + ' ' + myToday.getDate() + ' - ' + month[myToday.getUTCMonth()] + ' - ' + myToday.getFullYear();
 
-var myBioTemplate = (
+// function getLocation (location) {
+//     if (location) {
+//         return location;
+//     } else {
+//         return 'Unknown'
+//     };
+// };
+
+//using an arrow function
+const getLocation1 = (location) => {
+        if (location) {
+        return <p>Location1: {location}</p>;
+    }
+};
+
+//using a regular function
+function getLocation2 (location) {
+    if (location) {
+        return <p>Location2: {location}</p>;
+    }
+}
+
+const appTemplate = (
+    <div>
+        <h1>{appObj.title}</h1>
+        {appObj.subtitle && <p>{appObj.subtitle}</p>}
+        <p>{appObj.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>item one</li>
+            <li>item two</li>
+        </ol>
+    </div>
+);
+
+const bioTemplate = (
     <div>
         <h1>My Bio</h1>
-        <p>Name: {userDetailsName.toUpperCase() + '!'}</p>
-        <p>Age: {userDetailAge}</p>
-        <p>Occupation: {userDetailsOccupation}</p>
+        <p>Name: {bioObj.name ? bioObj.name.toUpperCase() + '!' : 'Anonymous'}</p>
+        {bioObj.age >= 18 && <p>Age: {bioObj.age}</p>}
+        <p>Occupation: {bioObj.occupation}</p>
+        {getLocation1(bioObj.Location1)}
+        {getLocation2(bioObj.Location2)}
         <p>As of: {userDetailsAsOf1}</p>
         <p>As of: {userDetailsAsOf2}</p>
     </div>
     );
 
-//These will run with or without Babel
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
-ReactDOM.render(myBioTemplate, appRoot);
+ReactDOM.render(bioTemplate, appRoot);
